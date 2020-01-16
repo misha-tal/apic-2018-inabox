bebbb5c451eee57cfa12fb4d7e12afbc0e2c88ee
Misha Tal <misha@demo.local>
2020-01-16 19:34:48 +0200

    sed fix

--- install-apic.sh
+++ install-apic.sh
@@ -17,17 +17,17 @@ ENDPOINT_PORTAL="portal.${FQDN_SUFFIX}"
 ENDPOINT_GW="gw.${FQDN_SUFFIX}"
 
 
-sed -i "#{{ENDPOINT_API_MANAGER_UI}}#${ENDPOINT_MANAGER}#g" apic-project/apiconnect-up.yml
-sed -i "#{{ENDPOINT_API_CLOUD_ADMIN_UI}}#${ENDPOINT_MANAGER}#g" apic-project/apiconnect-up.yml
-sed -i "#{{ENDPOINT_API_CONSUMER_API}}#${ENDPOINT_MANAGER}#g" apic-project/apiconnect-up.yml
-sed -i "#{ENDPOINT_API_PLATFORM_API}}#${ENDPOINT_MANAGER}#g" apic-project/apiconnect-up.yml
+sed -i "s#{{ENDPOINT_API_MANAGER_UI}}#${ENDPOINT_MANAGER}#g" apic-project/apiconnect-up.yml
+sed -i "s#{{ENDPOINT_API_CLOUD_ADMIN_UI}}#${ENDPOINT_MANAGER}#g" apic-project/apiconnect-up.yml
+sed -i "s#{{ENDPOINT_API_CONSUMER_API}}#${ENDPOINT_MANAGER}#g" apic-project/apiconnect-up.yml
+sed -i "s#{ENDPOINT_API_PLATFORM_API}}#${ENDPOINT_MANAGER}#g" apic-project/apiconnect-up.yml
 
-sed -i "#{{ENDPOINT_ANALYTICS_CLIENT}}#${ENDPOINT_ANALYTICS}#g" apic-project/apiconnect-up.yml
-sed -i "#{{ENDPOINT_ANALYTICS_INGESTION}}#${ENDPOINT_ANALYTICS}#g" apic-project/apiconnect-up.yml
+sed -i "s#{{ENDPOINT_ANALYTICS_CLIENT}}#${ENDPOINT_ANALYTICS}#g" apic-project/apiconnect-up.yml
+sed -i "s#{{ENDPOINT_ANALYTICS_INGESTION}}#${ENDPOINT_ANALYTICS}#g" apic-project/apiconnect-up.yml
 
-sed -i "#{{PORTAL_ADMIN_ENDPOINT}}#${ENDPOINT_PORTAL}#g" apic-project/apiconnect-up.yml
-sed -i "#{{PORTAL_WWW_ENDPOINT}}#${ENDPOINT_PORTAL}#g" apic-project/apiconnect-up.yml
+sed -i "s#{{PORTAL_ADMIN_ENDPOINT}}#${ENDPOINT_PORTAL}#g" apic-project/apiconnect-up.yml
+sed -i "s#{{PORTAL_WWW_ENDPOINT}}#${ENDPOINT_PORTAL}#g" apic-project/apiconnect-up.yml
 
-sed -i "#{{ENDPOINT_API_GATEWAY}}#${ENDPOINT_GW}#g" apic-project/apiconnect-up.yml
-sed -i "#{{ENDPOINT_APIC_GATEWAY_SERVICE}}#${ENDPOINT_GW}#g" apic-project/apiconnect-up.yml
+sed -i "s#{{ENDPOINT_API_GATEWAY}}#${ENDPOINT_GW}#g" apic-project/apiconnect-up.yml
+sed -i "s#{{ENDPOINT_APIC_GATEWAY_SERVICE}}#${ENDPOINT_GW}#g" apic-project/apiconnect-up.yml
 
