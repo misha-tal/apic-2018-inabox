084857ff5c61f2d3e9a0ef7c8dc57c682064fb2a
Misha Tal <misha@demo.local>
2020-01-16 19:47:17 +0200

    small fixes

--- install-apic.sh
+++ install-apic.sh
@@ -2,9 +2,15 @@
 
 
 # modify apic project template
-
-FQDN_SUFFIX="$1"
+EXTERNAL_IP_ADDRESS="$1"
 if [[ -z "$1" ]]
+then
+	echo "Missing parameter - IP address, e.g. $0 172.17.20.100"
+	exit 1
+fi
+
+FQDN_SUFFIX="$2"
+if [[ -z "$2" ]]
 then
 	echo "Missing parameter - fqdn suffix, e.g. $0 test-env.demo.com"
 	exit 1
@@ -17,10 +23,19 @@ ENDPOINT_PORTAL="portal.${FQDN_SUFFIX}"
 ENDPOINT_GW="gw.${FQDN_SUFFIX}"
 
 
+if grep --silent $ENDPOINT_MANAGER /etc/hosts
+then
+    echo "Host file contains apic entries."
+else
+    echo "Updating hosts file"
+    echo "$EXTERNAL_IP_ADDRESS   $ENDPOINT_MANAGER $ENDPOINT_ANALYTICS $ENDPOINT_PORTAL $ENDPOINT_GW"
+fi
+
+
 sed -i "s#{{ENDPOINT_API_MANAGER_UI}}#${ENDPOINT_MANAGER}#g" apic-project/apiconnect-up.yml
 sed -i "s#{{ENDPOINT_API_CLOUD_ADMIN_UI}}#${ENDPOINT_MANAGER}#g" apic-project/apiconnect-up.yml
 sed -i "s#{{ENDPOINT_API_CONSUMER_API}}#${ENDPOINT_MANAGER}#g" apic-project/apiconnect-up.yml
-sed -i "s#{ENDPOINT_API_PLATFORM_API}}#${ENDPOINT_MANAGER}#g" apic-project/apiconnect-up.yml
+sed -i "s#{{ENDPOINT_API_PLATFORM_API}}}#${ENDPOINT_MANAGER}#g" apic-project/apiconnect-up.yml
 
 sed -i "s#{{ENDPOINT_ANALYTICS_CLIENT}}#${ENDPOINT_ANALYTICS}#g" apic-project/apiconnect-up.yml
 sed -i "s#{{ENDPOINT_ANALYTICS_INGESTION}}#${ENDPOINT_ANALYTICS}#g" apic-project/apiconnect-up.yml
@@ -31,3 +46,4 @@ sed -i "s#{{PORTAL_WWW_ENDPOINT}}#${ENDPOINT_PORTAL}#g" apic-project/apiconnect-
 sed -i "s#{{ENDPOINT_API_GATEWAY}}#${ENDPOINT_GW}#g" apic-project/apiconnect-up.yml
 sed -i "s#{{ENDPOINT_APIC_GATEWAY_SERVICE}}#${ENDPOINT_GW}#g" apic-project/apiconnect-up.yml
 
+(cd apic-project; ../apicup-tools/apicup --accept-license subsys get manager --validate)
